065930d254a4421521a1401d86b461c566c0e2ca
coon3300 <coon3300@gmail.com>
2024-08-13 11:37:17 +0900

    커밋 충돌 제거

--- RnY/src/main/java/co/rny/control/QnaListControl.java
+++ RnY/src/main/java/co/rny/control/QnaListControl.java
@@ -22,7 +22,7 @@ public class QnaListControl implements Control {
 		QnaListService qvc = new QnaListServiceImpl();
 		int qnaCnt = qvc.qnaCnt();
 		
-		PageDTO paging = new PageDTO(Integer.parseInt(page), qnaCnt);
+		PageDTO paging = new PageDTO(Integer.parseInt(page), qnaCnt, 3);
 		List<QnaListVO> list = qvc.qnaPaging(Integer.parseInt(page));
 		req.setAttribute("qnaPaging", list); // QnA페이징
 		
